d1b2f89c4042eda3c98da3a4bcb63d2569106e41
leemyunggyu <jaylee@hatiolab.com>
2019-04-09 15:02:21 +0900

    pinterest 스타일뷰 적용
    TODO: 각 타일들의 높이를 결정하는 값 하드코딩 되어 있는것 수정

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,12 +6,33 @@ export default class MenuComponent extends connect(store)(LitElement) {
   static get styles() {
     return [
       css`
+        :host {
+          display: block;
+          box-sizing: border-box;
+        }
         .nav {
           list-style: none;
           padding: 0;
           margin: 0;
           display: flex;
-          flex-direction: row;
+        }
+        #main {
+          display: flex;
+          flex-flow: row wrap;
+          justify-content: center;
+        }
+        #main > ul {
+          padding: 0;
+          margin: 0;
+          list-style: none;
+          display: flex;
+          flex-direction: column;
+          width: var(--menu-list-column-width);
+          margin: var(--menu-list-column-margin);
+        }
+        #main > ul > li {
+          border: 1px solid #ccc;
+          margin: var(--menu-list-item-margin);
         }
       `
     ]
@@ -21,7 +42,12 @@ export default class MenuComponent extends connect(store)(LitElement) {
     return {
       _menuId: String,
       _menus: Array,
-      _currentMenu: Object
+      _currentMenu: Object,
+      columnWidth: Number,
+      columnCount: Number,
+      items: Array,
+      _columnCount: Number,
+      _columns: Array
     }
   }
 
@@ -32,10 +58,41 @@ export default class MenuComponent extends connect(store)(LitElement) {
     this._currentMenu = {
       children: []
     }
+
+    this.columnWidth = 320
+    this.maxColumnCount = 5
+    this.columnGap = 5
+    this.itemGap = 5
+    this.items = []
+    this._columnCount = 1
+
+    this._columns = []
+  }
+
+  firstUpdated() {
+    var ro = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        const cr = entry.contentRect
+        this.calculateColumnCount()
+        this.distributeColumnItems()
+      }
+    })
+
+    // Observe one or multiple elements
+    ro.observe(this.shadowRoot.getElementById('main'))
   }
 
   render() {
     return html`
+      <style>
+        :host {
+          --menu-list-item-margin: ${this.itemGap}px 0;
+          --menu-list-column-margin: 0 ${this.columnGap}px;
+          --menu-list-column-width: ${this.columnWidth}px;
+        }
+      </style>
+      <page-toolbar></page-toolbar>
+
       <ul class="nav">
         ${this._menus.map(
           (menu, idx) => html`
@@ -44,25 +101,51 @@ export default class MenuComponent extends connect(store)(LitElement) {
         )}
       </ul>
 
-      <ul>
-        ${this._currentMenu.children.map(c => {
-          console.log(c)
-          return html`
-            ${c.routingType.toUpperCase() === 'RESOURCE'
-              ? html`
-                  <li>
-                    <a href="${`resource-form-main/${c.menuId}`}">${c.name}</a>
-                  </li>
-                `
-              : html`
-                  <li>
-                    <a href=${`/${c.pageName}`}>${c.name}</a>
-                  </li>
-                `}
+      <section id="main">
+        ${this._columns.map(
+          c => html`
+            <ul>
+              ${c.map(
+                (item, idx) =>
+                  html`
+                    <li style="height: ${item.height}px">
+                      ${c[idx].routingType.toUpperCase() === 'RESOURCE'
+                        ? html`
+                            <a href="/resource-form-main/${c[idx].menuId}">${c[idx].name}</a>
+                          `
+                        : html`
+                            <a href="${c[idx].pageName}">${c[idx].name}</a>
+                          `}
+                    </li>
+                  `
+              )}
+            </ul>
           `
-        })}
-      </ul>
+        )}
+      </section>
     `
+  }
+
+  distributeColumnItems() {
+    var columns = []
+
+    this.items.forEach((item, idx) => {
+      if (!columns[idx % this._columnCount]) columns[idx % this._columnCount] = []
+
+      columns[idx % this._columnCount].push(item)
+    })
+
+    this._columns = columns
+    this.requestUpdate()
+  }
+
+  calculateColumnCount() {
+    var mainEl = this.shadowRoot.getElementById('main')
+    if (!mainEl) return
+    this._columnCount = Math.min(
+      Math.floor(mainEl.clientWidth / (this.columnWidth + this.columnGap * 2)),
+      this.maxColumnCount
+    )
   }
 
   stateChanged(state) {
@@ -70,6 +153,13 @@ export default class MenuComponent extends connect(store)(LitElement) {
     this._menus = state.baseMenu.menus
     this._currentMenu = this._menus[this._menuId || 0] || {
       children: []
+    }
+
+    this.items = this._currentMenu.children || []
+    if (this.items.length > 0) {
+      this.items.forEach(item => (item.height = Math.round(Math.random() * 400) + 100)) // TODO: 메뉴별 크기 하드코딩 제거
+      this.calculateColumnCount()
+      this.distributeColumnItems()
     }
   }
 }
