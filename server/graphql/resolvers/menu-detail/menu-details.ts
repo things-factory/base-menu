c6d1451012546af67f2dc415752c2207c10ff3e5
leemyunggyu <jaylee@hatiolab.com>
2019-06-03 15:08:19 +0900

    v0.1.22

--- server/graphql/resolvers/menu-detail/menu-details.ts
+++ server/graphql/resolvers/menu-detail/menu-details.ts
@@ -1,9 +1,9 @@
 import { getRepository } from 'typeorm'
 import { MenuDetail } from '../../../entities'
-import { ListParams, buildQuery } from '@things-factory/shell'
+import { ListParam, buildQuery } from '@things-factory/shell'
 
 export const menuDetailsResolver = {
-  async menuDetails(_: any, params: ListParams, context: any) {
+  async menuDetails(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(MenuDetail).createQueryBuilder()
     buildQuery(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
--- server/graphql/resolvers/menu/menus.ts
+++ server/graphql/resolvers/menu/menus.ts
@@ -1,9 +1,9 @@
-import { buildQuery, ListParams } from '@things-factory/shell'
+import { buildQuery, ListParam } from '@things-factory/shell'
 import { getRepository } from 'typeorm'
 import { Menu } from '../../../entities'
 
 export const menusResolver = {
-  async menus(_: any, params: ListParams, context: any) {
+  async menus(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(Menu).createQueryBuilder()
     buildQuery(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
