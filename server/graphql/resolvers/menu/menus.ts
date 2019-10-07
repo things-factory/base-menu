95f79fdc703d584723237876297ad2e6c002eb2c
Hearty Oh <heartyoh@hatiolab.com>
2019-10-07 11:40:22 +0800

    Update menus.ts

--- server/graphql/resolvers/menu/menus.ts
+++ server/graphql/resolvers/menu/menus.ts
@@ -7,10 +7,28 @@ export const menusResolver = {
     const convertedParams = convertListParams(params)
     convertedParams.where.domain = context.state.domain
 
-    const [items, total] = await getRepository(Menu).findAndCount({
+    var [items, total] = await getRepository(Menu).findAndCount({
       ...convertedParams,
-      relations: ['domain', 'parent', 'childrens', 'buttons', 'columns', 'creator', 'updater']
+      relations: ['domain', 'parent', 'childrens', 'buttons', 'columns', 'creator', 'updater'],
+      order: {
+        rank: 'ASC'
+      }
     })
+
+    items = items
+      .map((groupMenu: Menu) => {
+        return {
+          ...groupMenu,
+          childrens: groupMenu.childrens
+            .filter(
+              (subMenu: Menu) =>
+                (!subMenu.hiddenFlag && userRoles.includes(subMenu.category)) ||
+                (!subMenu.hiddenFlag && !subMenu.category)
+            )
+            .sort((a: Menu, b: Menu) => a.rank - b.rank)
+        }
+      })
+      .filter((groupMenu: Menu) => groupMenu.childrens && groupMenu.childrens.length)
 
     return { items, total }
   }
